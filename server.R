54882b7d1c98efa96371120f2c14d21fb1d35c9d
Sudhir Voleti <sudhir_voleti@isb.edu>
2025-03-14 16:02:30 +0530

    Update server.R

--- server.R
+++ server.R
@@ -13,15 +13,18 @@ server <- function(input, output) {
     raw_text <- input$raw_text
 
     if (nchar(raw_text) > 0) {
+      # Remove HTML tags
+      clean_text <- str_replace_all(raw_text, "<[^>]+>", " ")
+
       # Split text into paragraphs (or documents)
-      paragraphs <- str_split(raw_text, "\\n\\n")[[1]]
+      paragraphs <- str_split(clean_text, "\\n\\n")[[1]]
 
       # Sentence tokenization and sentiment analysis
       sentences_df <- tibble(doc_index = rep(1:length(paragraphs), times = sapply(str_split(paragraphs, "(?<=[.!?])\\s+"), length)),
                                sentence = unlist(str_split(paragraphs, "(?<=[.!?])\\s+"))) %>%
         mutate(sentence = str_trim(sentence)) %>%
         filter(nchar(sentence) > 0) %>%
-        unnest_tokens(word, sentence, token="words", drop=FALSE) %>% # added drop=FALSE to keep sentence column
+        unnest_tokens(word, sentence, token="words", drop=FALSE) %>%
         inner_join(get_sentiments("afinn"), by = "word") %>%
         group_by(doc_index, sentence) %>%
         summarise(sentiment_score = sum(value), .groups = "drop")
@@ -29,12 +32,12 @@ server <- function(input, output) {
       # Corpus Summary
       num_docs <- length(paragraphs)
       num_sentences <- nrow(sentences_df)
-      num_words <- str_count(raw_text, "\\w+")
+      num_words <- str_count(clean_text, "\\w+")
 
       output$corpus_summary <- renderPrint({
-        paste("Number of Documents/Paragraphs:", num_docs, "\n",
+        cat(paste("Number of Documents/Paragraphs:", num_docs, "\n",
               "Number of Sentences:", num_sentences, "\n",
-              "Number of Words:", num_words)
+              "Number of Words:", num_words))
       })
 
       # Sentiment Table
