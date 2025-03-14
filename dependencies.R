8dee41b8ce8f10dca81590b0b5c71c4beacc9111
Sudhir Voleti <sudhir_voleti@isb.edu>
2025-03-14 12:54:47 +0530

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -8,10 +8,14 @@ check_and_install <- function(package_name) {
   if (!requireNamespace(package_name, quietly = TRUE)) {
     install.packages(package_name)
   }
-  library(package_name)
-}
-
+  
 # Install and load required packages
 for (pkg in required_packages) {
   check_and_install(pkg)
 }
+
+# Load required libraries directly
+library('shiny')
+library('readr')
+library('dplyr')
+library('stringr')
