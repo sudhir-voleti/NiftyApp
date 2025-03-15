a0c320a4cdd8b5258999e5dd21a81d09157d127a
Sudhir Voleti <sudhir_voleti@isb.edu>
2025-03-15 06:07:23 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,29 +1,22 @@
 # ui.R
-library(shiny)
-library(plotly)
 
-ui <- fluidPage(
+source("dependencies.R")
+
+shinyUI(fluidPage(
   titlePanel("Text Analysis App"),
+
   sidebarLayout(
     sidebarPanel(
-      textAreaInput("raw_text", "Paste Raw Text Here:", rows = 5),
+      textAreaInput("raw_text", "Paste Raw Text Here:", rows = 10),
       actionButton("run_analysis", "Run Analysis")
     ),
+
     mainPanel(
-      tabsetPanel(
-        tabPanel("Summary",
-                 h4("Corpus Summary:"),
-                 verbatimTextOutput("corpus_summary"),
-                 h4("Sentence Sentiment Analysis:"),
-                 DT::dataTableOutput("sentiment_table")
-        ),
-        tabPanel("sentiPlot",
-                 plotlyOutput("sentiment_plot")
-        ),
-        tabPanel("freq distbns",
-                 plotlyOutput("freq_dist_plot")
-        )
-      )
+      h4("Summary Statistics"),
+      verbatimTextOutput("summary_output"),
+
+      h4("Sentence Sentiment Analysis"),
+      DTOutput("sentiment_table")
     )
   )
-)
+))
