47131dfc6fd5f62fa0bcddec614e5a3ac6f7e0bc
Sudhir Voleti <sudhir_voleti@isb.edu>
2025-03-15 06:21:27 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,21 +2,25 @@
 
 source("dependencies.R")
 
-shinyUI(fluidPage(
-  titlePanel("Text Analysis App"),
-
-  sidebarLayout(
-    sidebarPanel(
-      textAreaInput("raw_text", "Paste Raw Text Here:", rows = 10),
-      actionButton("run_analysis", "Run Analysis")
-    ),
-
-    mainPanel(
-      h4("Summary Statistics"),
-      verbatimTextOutput("summary_output"),
-
-      h4("Sentence Sentiment Analysis"),
-      DTOutput("sentiment_table")
+shinyUI(navbarPage("Text Analysis App",
+  tabPanel("Text Analysis",
+    sidebarLayout(
+      sidebarPanel(
+        textAreaInput("raw_text", "Paste Raw Text Here:", rows = 10),
+        actionButton("run_analysis", "Run Analysis")
+      ),
+      mainPanel(
+        h4("Summary Statistics"),
+        htmlOutput("summary_output"),
+        h4("Sentence Sentiment Analysis"),
+        DTOutput("sentiment_table")
+      )
     )
+  ),
+  tabPanel("sentiPlot",
+    plotlyOutput("sentiment_plot")
+  ),
+  tabPanel("Histogram",
+    plotOutput("sentiment_histogram")
   )
 ))
