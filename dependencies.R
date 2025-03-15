fbeee60d863a0732d74f40273a7a24ca53016185
Sudhir Voleti <sudhir_voleti@isb.edu>
2025-03-15 16:21:57 +0530

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -1,14 +1,15 @@
 # dependencies.R
 
 # List of required packages
-required_packages <- c("shiny", "stringr", "rvest", "httr", "tidytext", "dplyr", "sentimentr", "DT", "plotly", "ggplot2")
+required_packages <- c("shiny", "quantmod", "DT", "lubridate", "dplyr")
 
-# Function to check, install, and load packages
-load_packages <- function(pkg_list) {
-  new_packages <- pkg_list[!(pkg_list %in% installed.packages()[,"Package"])]
-  if(length(new_packages)) install.packages(new_packages)
-  for(pkg in pkg_list) library(pkg, character.only = TRUE)
+# Function to check and install packages
+install_if_missing <- function(pkg) {
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg, dependencies = TRUE)
+    library(pkg, character.only = TRUE)
+  }
 }
 
-# Load the required packages
-load_packages(required_packages)
+# Install and load all required packages
+lapply(required_packages, install_if_missing)
